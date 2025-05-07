46a550bed73efc7fe1ff0fa387b20f98c5d36e4e
kunalg <kunalgaikwad5533@gmail.com>
2025-05-07 11:55:09 +0530

    show.ejs updation

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+app.use('/uploads', express.static('uploads'));
 
 // Models
 const Listing = require("./models/listing.js");
